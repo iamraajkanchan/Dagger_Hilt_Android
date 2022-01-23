c497f44662836269bcde4e1a39998bcf9bcc8fd1
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-01-23 23:42:41 +0530

    Example of Module Injection

--- app/src/main/java/com/example/dagger_hilt_android/model/Battery.kt
+++ app/src/main/java/com/example/dagger_hilt_android/model/Battery.kt
@@ -3,11 +3,11 @@ package com.example.dagger_hilt_android.model
 import android.util.Log
 import com.example.dagger_hilt_android.utils.Utils
 
-class Battery(val cobalt : Cobalt , val lithium : Lithium)
+class Battery(private val cobalt : Cobalt , private val lithium : Lithium)
 {
     // If you do not own this class then you can't annotate this class with @Inject
     init
     {
-        Log.d(Utils.TAG , "Battery :: $this")
+        Log.d(Utils.TAG , "Battery :: $this, Cobalt :: $cobalt, Lithium :: $lithium")
     }
 }
