c14ecb0a19a4e2799a0a535cb38bd898111f14ae
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-01-26 10:57:17 +0530

    Updated on 26th Jan 2022

--- app/src/main/java/com/example/dagger_hilt_android/model/Battery.kt
+++ app/src/main/java/com/example/dagger_hilt_android/model/Battery.kt
@@ -6,6 +6,7 @@ import javax.inject.Inject
 
 class Battery(private var cobalt : Cobalt , private var lithium : Lithium)
 {
+    /*If we don't own the class then we can't annotate the class with @Inject*/
     init
     {
         Log.d(Utils.TAG , "Battery :: $this, Cobalt :: $cobalt, Lithium :: $lithium")
--- app/src/main/java/com/example/dagger_hilt_android/model/Cobalt.kt
+++ app/src/main/java/com/example/dagger_hilt_android/model/Cobalt.kt
@@ -6,6 +6,7 @@ import javax.inject.Inject
 
 class Cobalt
 {
+    /*If we don't own the class then we can't annotate the class with @Inject*/
     init
     {
         Log.d(Utils.TAG , "Cobalt :: $this")
