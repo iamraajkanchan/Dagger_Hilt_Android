c50fcfe2cee5f4fef4fd6be36a782e2f0137e375
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-06-20 21:14:15 +0530

    PostActivity.kt | Updated

--- app/src/main/java/com/example/dagger_hilt_android/ui/post/PostActivity.kt
+++ app/src/main/java/com/example/dagger_hilt_android/ui/post/PostActivity.kt
@@ -18,12 +18,10 @@ class PostActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
 
-        viewModel.insertAll(
-            Post(0 , "Dagger-Hilt" , "Learn Dagger Hilt") ,
+        viewModel.insertAll(Post(0 , "Dagger-Hilt" , "Learn Dagger Hilt") ,
             Post(0 , "Java" , "Learn Java") ,
             Post(0 , "Android" , "Learn Android") ,
-            Post(0 , "Kotlin" , "Learn Kotlin")
-        )
+            Post(0 , "Kotlin" , "Learn Kotlin"))
 
         viewModel.postLiveData.observe(this) {
             Log.d(Utils.TAG , "PostActivity :: onCreate :: $it")
